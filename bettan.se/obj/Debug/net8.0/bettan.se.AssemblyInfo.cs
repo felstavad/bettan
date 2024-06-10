d6ed4b9c94f1da2861444be70d0ecbf23429c0de
Elisabeth Hällsten <elisabeth.hallsten@gmail.com>
2024-06-10 14:05:47 +0200

    Changed wwwroot to root

--- bettan.se/obj/Debug/net8.0/bettan.se.AssemblyInfo.cs
+++ bettan.se/obj/Debug/net8.0/bettan.se.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("bettan.se")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d094299eb54b19b44ee2ec3c633ac9c818675567")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06e79ed5d7f96e5d6d91d3dad5954dec26520f0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("bettan.se")]
 [assembly: System.Reflection.AssemblyTitleAttribute("bettan.se")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
