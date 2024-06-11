5fc57d73e3547c7293ac65614a8a8467d690b818
Elisabeth Hällsten <elisabeth.hallsten@gmail.com>
2024-06-11 15:05:01 +0200

    vetintevadjaggör

--- bettan.se/obj/Debug/net8.0/bettan.se.AssemblyInfo.cs
+++ bettan.se/obj/Debug/net8.0/bettan.se.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("bettan.se")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8858f3acbc2979aae4372f751b084ac995c4fef8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f4ea77aafffebe6ce7817e45bfcf12d8b54bf43")]
 [assembly: System.Reflection.AssemblyProductAttribute("bettan.se")]
 [assembly: System.Reflection.AssemblyTitleAttribute("bettan.se")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
